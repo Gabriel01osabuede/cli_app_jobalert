ca93f31046fdf19aee907ffc7dd9d390bcb3b697
Gabriel01osabuede <51996566+Gabriel01osabuede@users.noreply.github.com>
2020-05-28 13:32:10 +0100

    added few insert statements manually

--- cli_jobalert_app/job_house.py
+++ cli_jobalert_app/job_house.py
@@ -276,6 +276,144 @@ def databaseConnection():
                             "COMPANYNAME TEXT NOT NULL,"
                             "TIMEPOSTED TEXT NOT NULL)")
 
+    # RUNNING A INSERT STATEMENT MANUALLY TO TEST OUT THE DATABASE..
+    Sqlite_insert_query = """INSERT INTO USER_POSTS(NAME, JOBTYPE, JOBDESCRIPTION, LOCATION, COMPANYNAME, TIMEPOSTED)
+                           VALUES (?,?,?,?,?,?) """
+
+    records_to_insert = [('SHOLA','ICT Officer (SAP MM Deployment and Support)'
+                      ,'We are looking for a UI/UX Designer to turn our'
+                       'software into easy-to-use products for our clients',
+                      'LAGOS','Oawtechnologies', time.asctime(time.localtime(time.time()))),
+                     ('TUNDE','Sales Rep',
+                      'Our company is looking for a Sales Representative to be responsible for '
+                      'generating leads and meeting sales goals. Duties will include '
+                      'sales presentations and product demonstrations, as well as negotiating '
+                      'contracts with potential clients.',
+                      'LAGOS','Oawtechnologies',time.asctime(time.localtime(time.time()))),
+                     ('ADEBISI','Tech Project Manager',
+                      'We are looking for a detail-oriented Technical Project '
+                      'Manager to oversee all project operations from inception '
+                      'to execution. The Technical Project Manager is responsible for maintaining '
+                      'budgets, enforcing deadlines and supervising team members','LAGOS',
+                      'Oawtechnologies',time.asctime(time.localtime(time.time()))),
+                     ('SANDRA','Backend Developer - Remote',
+                      'We are looking for an experienced Back-end developer to join our IT team. '
+                      'You will be responsible for the server side of our web applications. '
+                      'If you have excellent programming skills and a passion for developing applications or '
+                      'improving existing ones, we would like to meet you.','LAGOS',
+                      'Oawtechnologies', time.asctime(time.localtime(time.time()))),
+                     ('KOLA','Creative Graphic Designer',
+                      'Illustrating concepts by designing examples of art arrangement,'
+                      ' size, type size and style and submitting them for approval','LAGOS',
+                      'Ryteprint',time.asctime(time.localtime(time.time()))),
+                     ('SHULAMITE','Social media / Sales Intern (Female Preffered)',
+                      'Erocraves is looking for an enthusiastic social media/sales '
+                      'intern who will contribute to increasing the company sales.',
+                      'LAGOS','Erocraves',time.asctime(time.localtime(time.time()))),
+                     ('TOBI','Sales & Marketing Executive',
+                      'Hoduns Foods Ventures is looking to recruit suitably qualified'
+                      ' candidate to fill the position of Sales & Marketing Executive',
+                      'LAGOS','Hoduns Foods Ventures', time.asctime(time.localtime(time.time()))),
+                     ('JASON','Farm Manager',
+                      'We are searching for a Farm Manager with good business sense to join our team.',
+                      'Abeokuta & Ogun State','V&l limited',time.asctime(time.localtime(time.time()))),
+                     ('DURELO','Executive Director',
+                      'Seeking executive director responsible for overseeing the administration,'
+                      ' programs and strategic plan of the organization.',
+                      'LAGOS','GACO INDUSTRIES LIMITED',time.asctime(time.localtime(time.time()))),
+                     ('FAVOR','Director of Information Technology',
+                      'Seeking an IT director with leadership skills, project management '
+                      'skills. At least 5 years experience with executive management position',
+                      'LAGOS','GACO INDUSTRIES LIMITED', time.asctime(time.localtime(time.time()))),
+                     ('TAYO','Office Assistant',
+                      'We are currently recruiting for this position',
+                      'ABUJA','KINABS INFINITI NIG LTD',time.asctime(time.localtime(time.time()))),
+                     ('ABRAHAM','Front End Developer',
+                      'Design, develop and test (unit, integration, component and system) '
+                      'assigned tasks in accordance with the agreed architecture, solution design',
+                      'LAGOS','Cognetiks Consulting',time.asctime(time.localtime(time.time()))),
+                     ('LINDA','VP, Operations',
+                      'We are currently seeking a highly-motivated professional'
+                      ' to (1) develop, review, and implement new processes, designed to support '
+                      'sustainable growth; and (2) oversee all elements of live (client) workflow execution.',
+                      'LAGOS','Anonymous Employer', time.asctime(time.localtime(time.time()))),
+                     ('SUZY','Front End Developer',
+                      'Experienced Front End Developer required to develop web and mobile applications',
+                      'LAGOS','Anonymous Employer',time.asctime(time.localtime(time.time()))),
+                     ('WILLIAMS','UI / UX Designer',
+                      'UI/UX Designer needed. If you have a portfolio of professional design '
+                      'projects that includes work with web/mobile applications, '
+                      'we’d like to meet you.',
+                      'LAGOS','Anonymous Employer',time.asctime(time.localtime(time.time()))),
+                     ('CHARLES','Coordinating Desk Officer',
+                      'We are currently recruiting for this position',
+                      'ABUJA',
+                      'Deutsche Gesellschaft fuer Internationale Zusammenarbeit (GIZ) GmbH',
+                      time.asctime(time.localtime(time.time()))),
+                     ('REMILEKUN','Sales Manager',
+                      'Seeking for an ambitious, innovative, forward-thinking and highly'
+                      ' motivated individual who will move the company forward with strong leadership,'
+                      ' committed people management and a commercial focus on project sourcing,'
+                      ' delivery and profitability','LAGOS',
+                      'RyteGate Construction & Technologies Ltd.',time.asctime(time.localtime(time.time()))),
+                     ('ISABELLA','Special Advisor to ECOWAS Commission',
+                      'We are currently recruiting for this position','ABUJA',
+                      'Deutsche Gesellschaft fuer Internationale Zusammenarbeit (GIZ) GmbH',
+                      time.asctime(time.localtime(time.time()))),
+                     ('BRANDY','Quality Control / Technical Officer',
+                      'Quality control and technical officer position in a '
+                      'manufacturing company based at Lagos/Ibadan expressway.',
+                      'LAGOS','STOVA INDUSTRIES LIMITED', time.asctime(time.localtime(time.time()))),
+                     ('GABRIEL','Back-end Developer',
+                      'Responsible for analysis, design (UML), development and testing '
+                      'of AET internal products and also be accountable for building'
+                      ' RESTful web-facing applications and optimized logic to facilitate'
+                      ' the organizations interaction with its client.',
+                      'LAGOS','ActivEdge Technologies Limited',time.asctime(time.localtime(time.time()))),
+                     ('YVONNE','BIOMEDICAL ENGINEER',
+                      'The Biomedical Engineer would be responsible for the design,'
+                      ' development and implementation of biomedical equipment'
+                      ' and care delivery systems as well as the provision of support'
+                      ' in the delivery of Medical IT solutions to clients.','LAGOS',
+                      'Medcourt Pharmacy',time.asctime(time.localtime(time.time()))),
+                     ('AGNES','Sales Manager','We are currently recruiting for this position',
+                      'LAGOS','Bemas Technologies Limited',time.asctime(time.localtime(time.time()))),
+                     ('FAMOUS','Content Creator/Writer (Internship)',
+                      'We are looking for a Content Creator to write and publish '
+                      'various types of pieces for our company’s web pages,'
+                      ' like articles, ebooks and social media','KWARA','Anter Technologies',
+                      time.asctime(time.localtime(time.time()))),
+                     ('HELEN','Marketing Manager',
+                      'This position has overall oversight and responsibility for,'
+                      ' marketing and generating revenue by increasing sales through'
+                      ' successful application of marketing strategies and coordinating the'
+                      ' marketing & Sales force of the firm.','LAGOS','Anonymous Employer'
+                      ,time.asctime(time.localtime(time.time()))),
+                     ('DAVID','Social Media Manager (Internship)',
+                      'We are currently hiring for this position.',
+                      'KWARA','Anter Technologies', time.asctime(time.localtime(time.time()))),
+                     ('CHUCK','Civil/Structural Engineer',
+                      'Vacancy For Civil/Structural Engineer at Haserv Enigeering Limited',
+                      'LAGOS','Haserv Engineering Limited',time.asctime(time.localtime(time.time()))),
+                     ('RAMOND','PMP Instructor',
+                      'We are seeking Project Management instructors and consultants'
+                      ' to work on an independent contractor basis.',
+                      'LAGOS','Anonymous Employer',time.asctime(time.localtime(time.time()))),
+                     ('UNICE','SQL Server Instructor','To teach both weekdays and weekends SQL Server courses.',
+                      'LAGOS','Anonymous Employer', time.asctime(time.localtime(time.time()))),
+                     ('TOYIN','Certified Cisco Academy Instructor',
+                      'The successful candidate will be required to teach introductory and advanced'
+                      ' computer networking courses in Cisco technologies and cybersecurity.',
+                      'LAGOS','Anonymous Employer',time.asctime(time.localtime(time.time()))),
+                     ('KEZZY','Database Engineer',
+                      'Database engineers write new database programs and maintain'
+                      ' existing programs to ensure they can handle the flow of traffic.',
+                      'LAGOS','Insight Apex Group of Companies',time.asctime(time.localtime(time.time()))),]
+
+    data = conn.executemany(Sqlite_insert_query, records_to_insert)
+    conn.commit()
+
+
     conn.close()
 
 home()
